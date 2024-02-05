942942fd9263184e586e82115cdb3d68804214ff
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-05 01:26:55 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -25,19 +25,13 @@ int main(int argc, char* argv[]) {
 
         // Uncomment this to pass the first stage
         database_file.seekg(16);  // Skip the first 16 bytes of the header
-        
         char buffer[2];
         database_file.read(buffer, 2);
-        
         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        
-      
-        
         database_file.seekg(HEADER_SIZE+3);
         database_file.read(buffer, 2);
         unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         std::cout << "database page size: " << page_size << std::endl;
-1
         std::cout << "number of tables: " << num_table << std::endl;
         
 
