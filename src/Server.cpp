46df52fc25ec58ba5e1fd9d7aaac88aa300990a3
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-05 01:43:09 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -34,18 +34,31 @@ int main(int argc, char* argv[]) {
         unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         std::cout << "database page size: " << page_size << std::endl;
         std::cout << "number of tables: " << num_table << std::endl;
+    
+
+        
+    }
+    else if (command == ".tables") {
+        std::ifstream database_file(database_file_path, std::ios::binary);
+        if (!database_file) {
+            std::cerr << "Failed to open the database file" << std::endl;
+            return 1;
+        }
+
+        // Uncomment this to pass the first stage
+        database_file.seekg(16);  // Skip the first 16 bytes of the header
+        char buffer[2];
+        database_file.read(buffer, 2);
+        unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         database_file.seekg(HEADER_SIZE+3);
         database_file.read(buffer, 2);
+        unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+        std::cout << "database page size: " << page_size << std::endl;
+        std::cout << "number of tables: " << num_table << std::endl;
         database_file.seekg(HEADER_SIZE+pageHeader);
         char buff[2];
         database_file.read(buff, 2);
         std :: cout << buff << std::endl;
-
-
-        
-
-
-
 
 
     }
