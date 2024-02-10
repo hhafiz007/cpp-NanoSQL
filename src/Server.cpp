164cec2957691b376276a08697fbc94312f01c48
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-09 22:11:36 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -101,9 +101,10 @@ int main(int argc, char* argv[]) {
     // Output the number of bytes read
     std::cout << "Number of bytes read: " << bytes.size() << std::endl;
 
-  
+  int start = 108;
+    
 
-    printTables(bytes,num_table,int start = 108);
+    printTables(bytes,num_table,int start);
 
         // unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8))
     
