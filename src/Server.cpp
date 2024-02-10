7164d394947b9910d11f8a0224e13cd9c9211648
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-10 12:35:43 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -185,23 +185,13 @@ int main(int argc, char* argv[]) {
     std::string database_file_path = argv[1];
     std::string command = argv[2];
 
-    if (command == ".dbinfo") {
-        std::ifstream database_file(database_file_path, std::ios::binary);
+    std::ifstream database_file(database_file_path, std::ios::binary);
         if (!database_file) {
             std::cerr << "Failed to open the database file" << std::endl;
             return 1;
         }
 
-        // Uncomment this to pass the first stage
-        database_file.seekg(16);  // Skip the first 16 bytes of the header
-        char buffer[2];
-        database_file.read(buffer, 2);
-        unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        database_file.seekg(HEADER_SIZE+3);
-        database_file.read(buffer, 2);
-        unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        std::cout << "database page size: " << page_size << std::endl;
-        std::cout << "number of tables: " << num_table << std::endl;
+        
        
     // Get the file size
     database_file.seekg(0, std::ios::end);
@@ -215,10 +205,22 @@ int main(int argc, char* argv[]) {
     // Close the file
     database_file.close();
 
-    // Output the number of bytes read herea
-    std::cout << "Number of bytes read: " << bytes.size() << std::endl;
+    if (command == ".dbinfo") {
 
-    int start = 108;
+        // Uncomment this to pass the first stage
+        database_file.seekg(16);  // Skip the first 16 bytes of the header
+        char buffer[2];
+        database_file.read(buffer, 2);
+        unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+        database_file.seekg(HEADER_SIZE+3);
+        database_file.read(buffer, 2);
+        unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+        std::cout << "database page size: " << page_size << std::endl;
+        std::cout << "number of tables: " << num_table << std::endl;
+        
+
+    // Output the number of bytes read herea
+    
     
 
     
@@ -228,25 +230,7 @@ int main(int argc, char* argv[]) {
     }
     else if ( command == ".tables")
     {
-          std::ifstream database_file(database_file_path, std::ios::binary);
-        if (!database_file) {
-            std::cerr << "Failed to open the database file" << std::endl;
-            return 1;
-        }
-
-        char buffer[2];
-        database_file.seekg(HEADER_SIZE+3);
-        database_file.read(buffer, 2);
-        unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-       
-    // Get the file size
-    database_file.seekg(0, std::ios::end);
-    std::streampos fileSize = database_file.tellg();
-    database_file.seekg(0, std::ios::beg);
-
-    // Read the file into a vector of bytes
-    std::vector<char> bytes(fileSize);
-    database_file.read(bytes.data(), fileSize);
+        
 
     int start = 108;
         printTables(bytes,num_table,start);
