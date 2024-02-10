62394b76840b443269b287689c885f1714c18a11
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-10 02:47:03 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -61,7 +61,7 @@ int processVarInt(std::vector<char> &database_file ,unsigned short rowAddress){
         unsigned int result = 0;
         for ( int j = prev ; j < next; j++) {
             result <<= 8;
-            result |= int(database_file[j]);
+            result |= (database_file[j]);
         }
         std::cout << " indices" << prev <<"   "<< next<<" "<<rowAddress+totalBytes << "   " << result <<std:: endl;
         i = next;
