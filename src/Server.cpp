ece3f2e95cdca8ce4c180a96c379668299afad70
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-09 21:09:35 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -5,6 +5,15 @@
 const int HEADER_SIZE = 100;
 const int pageHeader = 8;
 
+struct schemaRow {
+    // Member variables
+    int type;
+    std::string name;
+    std::string tblName;
+    std::string rootPage;
+    std::string sql;
+};
+
 int main(int argc, char* argv[]) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std::cout << "Logs from your program will appear here" << std::endl;
@@ -34,6 +43,7 @@ int main(int argc, char* argv[]) {
         unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         std::cout << "database page size: " << page_size << std::endl;
         std::cout << "number of tables: " << num_table << std::endl;
+        printTables(database_file,num_table);
 
         // unsigned short num_table = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8))
     
@@ -43,4 +53,22 @@ int main(int argc, char* argv[]) {
     }
     
     return 0;
+}
+
+
+
+void printTables(std::ifstream database_file , unsigned short num_table) {
+    database_file.seekg(108); 
+    int totalBytes = 2*num_table;
+    char buffer[totalBytes];
+    database_file.read(buffer, totalBytes);
+    std::vector<unsigned short> cellAddress;
+
+    // Calculate factorial
+    for (int i = 0; i < num_table; i++) {
+        int startIndex = 2*i; 
+        unsigned short page_address = (static_cast<unsigned char>(buffer[startIndex+1]) | (static_cast<unsigned char>(buffer[startIndex]) << 8));
+        cellAddress.push_back(page_address);
+    }
+    std::cout << "adresses of " << cellAddress << std::endl;
 }
