c1cf2e3bcd6c3c15fb7ab120c0cd375a696b595d
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-10 10:44:43 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -119,7 +119,7 @@ void printTables(std::vector<char> &database_file , unsigned short num_table,int
         int startIndex = 2*i+start; 
         unsigned short page_address = (static_cast<unsigned char>(database_file[startIndex+1]) | (static_cast<unsigned char>(database_file[startIndex]) << 8));
         cellAddress.push_back(page_address);
-         std::cout << "Logs from your program will appear here" << page_address<<std::endl;
+         std::cout << "Logs from your program will appeaar here" << page_address<<std::endl;
         
 
     }
