c76c6c7fbc32edc6839909f8d3c73d192eccdf77
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-10 12:01:15 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -105,7 +105,7 @@ int getRowData(std::vector<char> &database_file , unsigned short rowAddress){
 
     for (int element : header) {
         std::cout <<"printing header element" <<element<< std:: endl;
-        element = std::ceil(x)((element -13)/2);
+        element = std::ceil((element -13)/2);
         int startByte = next;
         int endExclusive = next + element;
         std:: string currHeader;
