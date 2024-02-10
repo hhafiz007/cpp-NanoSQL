65f22d7c63e4d36ac40f1f45a6ec824e563dc20c
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-09 23:29:59 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -74,7 +74,7 @@ void printTables(std::vector<char> &database_file , unsigned short num_table,int
     }
 
     for (int i = 0; i < cellAddress.size(); ++i) {
-        start = getRowData(database_file,start);
+       getRowData(database_file,cellAddress[i]);
         break;
     }
 
