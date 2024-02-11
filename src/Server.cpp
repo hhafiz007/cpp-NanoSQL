149c2f357e732cee8e7db48290d7c205223e24de
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-11 00:34:28 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -348,6 +348,9 @@ void printSelectColumns(   std::vector <std::vector<std::string>> &tableData, st
     }
 
     for (int columnNamecur : columnIndices) {
+        if (columnIndices.size() == 1){
+            break;
+        }
         std :: cout << columnNamecur <<" printed column number" << std:: endl;
     }
 // kk
