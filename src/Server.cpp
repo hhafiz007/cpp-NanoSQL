997921ab58b5d3e90a4d91b68c6c4a7f0e84120b
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-11 00:25:20 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -358,12 +358,12 @@ void printSelectColumns(   std::vector <std::vector<std::string>> &tableData, st
         
         std :: string rowData;
 
-        rowData = tableData[columnIndices[0]];
+        rowData = tableData[j][columnIndices[0]];
         int k =1;
 
         for (k = 1; k < columnIndices.size();k+=1)
         {
-            rowData += '|'+tableData[columnIndices[k]];
+            rowData += '|'+tableData[j][columnIndices[k]];
         }
 
         std :: cout << rowData << std ::endl;
