2ee567cbec699cd78c8530589f0bfda99e21f194
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-17 15:45:53 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -194,7 +194,7 @@ void printTableLeafPage(std::vector<char> &database_file , unsigned short num_ta
     std::vector<unsigned short> cellAddress;
 
     if (debugStage){
-        std :: cout << "the start is"<<start<<"  ro ws "<< num_table << std::endl; 
+        std :: cout << "the start d is"<<start<<"  ro ws "<< num_table << std::endl; 
 
     }
 
