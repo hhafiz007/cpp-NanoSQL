6446c2b374e9f7ee4b77293e85e75f2784e7bea5
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-17 16:35:01 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -621,10 +621,10 @@ int main(int argc, char* argv[]) {
         int rootPage;
         std::vector<std::string> columnNames = getRootPage(schemaData,tableName,rootPage);
 
-         std::cerr << "Debug4000: " << rootPage << std::endl;
+         std::cerr << "Debug4000: " << rootPage <<"   "<<tableName std::endl;
         
         start = (rootPage-1)*4096;
-        std :: cerr <<rootPage << "back here  " << int(bytes[start]) << "     "<<page_size <<" ";
+        // std :: cerr <<rootPage << "back here  " << int(bytes[start]) << "     "<<page_size <<" ";
 
         unsigned short cellCount=(static_cast<unsigned char>(bytes[start+4]) | (static_cast<unsigned char>(bytes[start+3]) << 8));
         
