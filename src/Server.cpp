e055e90a5c8a1134b368d4c3fa1cd319139d9f10
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-17 15:49:13 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -441,7 +441,7 @@ MyTuple parseWhereFilter(std::string &query){
 
 
     size_t found = query.find("where");
-    std::cout << query << etd::endl;
+    std::cout << query << std::endl;
     MyTuple ans;
     if (found != std::string::npos) {
         // std::vector<std::string> tokens = split(query, "WHERE");
