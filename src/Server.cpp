8578314d1c7cf99f968350d0fc1e3a88874c4514
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-17 15:34:44 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -610,7 +610,7 @@ int main(int argc, char* argv[]) {
 
         printTableLeafPage(bytes,cellCount,newAddress,tableData);
 
-        // std :: cout << "back here  " << start  <<" "; // change comments kk mm mm
+        // std :: cout << "back here  " << start  <<" "; // change comments kk mm mm ss
 
         printSelectColumns(tableData, columnNames , selectColumns,selectFilter);
 
