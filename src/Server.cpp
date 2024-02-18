587dec9cc93c212d1b773be956b756310c4b3745
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 13:29:51 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -347,7 +347,7 @@ std::vector<std::string> columnNames;
         contents += (*iter)[0].str().substr(1, (*iter)[0].str().size() - 2); // Remove surrounding parentheses
     }
     std::vector<std::string> tokens = split(contents, ',');
-    // std::cout << "contents  " << contents;
+    std::cerr << "contents  are" << contents;
     int index = 0;
 
     while( index <tokens.size()){
