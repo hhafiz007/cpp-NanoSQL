3eee44dea101ab610b4ce09af505f1e15d37798a
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 13:45:34 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -761,7 +761,7 @@ int main(int argc, char* argv[]) {
             for (std::string value : row) {
 
                 std::cerr << "index is "<<index<< <<value<<"  "; 
-                index+=1
+                index+=1;
             }
 
             std::cerr <<std::endl;
