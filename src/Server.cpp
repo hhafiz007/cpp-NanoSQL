48151b6863430fb68d6c4289b55b1f7de41318e6
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 18:24:26 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -361,9 +361,9 @@ void printTableLeafPage(std::vector<char> &database_file , unsigned short num_ta
     // std::cerr<<"Page start is "<< startAddress<<"  celllls are "<< num_table <<" addresses are"<<\
     cellAddress.size()<<std::endl;
 
-    //  if (tableData.size() > 56) {
-    //         break;
-    //     }
+     if (tableData.size() > 1) {
+            break;
+        }
         
 
     for (int i = 0; i < cellAddress.size(); ++i) {
@@ -371,9 +371,9 @@ void printTableLeafPage(std::vector<char> &database_file , unsigned short num_ta
     //  std::cerr << "processing row  " <<cellAddress[i]<<std::endl; 
         
        getRowData(database_file,cellAddress[i],tableData);
-    //    if (tableData.size() > 58) {
-    //         break;
-    //     }
+       if (tableData.size() > 2) {
+            break;
+        }
     
 
        
