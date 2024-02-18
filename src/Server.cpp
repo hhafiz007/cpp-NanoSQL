505ce2745617f01077fae7d34d1cd7c2b011bbe4
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 14:23:30 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -209,35 +209,31 @@ int  getLeafPage(std::vector<char> &database_file, int start,std::vector<uint32_
     if (int(pageType) == interiorTablePage) {
         unsigned short num_table = (static_cast<unsigned char>(database_file[pageStart+4]) | (static_cast<unsigned char>(database_file[pageStart+3]) << 8));
         start +=12;
-        
-        
-
-
         for (int i = 0; i < int(num_table); i++) {
 
        
 
-        
-        int startIndex = (2*i)+start; 
+                
+                int startIndex = (2*i)+start; 
 
-        unsigned short byte1 = static_cast<unsigned char>(database_file[startIndex]);
-        unsigned short byte2 = static_cast<unsigned char>(database_file[startIndex+1]);
-        unsigned short page_address =   ((byte1 << 8) | (byte2))+pageStart;
+                unsigned short byte1 = static_cast<unsigned char>(database_file[startIndex]);
+                unsigned short byte2 = static_cast<unsigned char>(database_file[startIndex+1]);
+                unsigned short page_address =   ((byte1 << 8) | (byte2))+pageStart;
 
 
-        uint32_t result1 = static_cast<uint32_t>(database_file[page_address]);
-        uint32_t result2 = static_cast<uint32_t>(database_file[page_address+1]);
-        uint32_t result3 = static_cast<uint32_t>(database_file[page_address+2]);
-        uint32_t result4 = static_cast<uint32_t>(database_file[page_address+3]);
+                uint32_t result1 = static_cast<uint32_t>(database_file[page_address]);
+                uint32_t result2 = static_cast<uint32_t>(database_file[page_address+1]);
+                uint32_t result3 = static_cast<uint32_t>(database_file[page_address+2]);
+                uint32_t result4 = static_cast<uint32_t>(database_file[page_address+3]);
 
-        uint32_t result =   ((result1 << 24) | (result2 << 16) | (result3 << 8) | (result4));
-        int nextAdress = (result-1)*4096;
+                uint32_t result =   ((result1 << 24) | (result2 << 16) | (result3 << 8) | (result4));
+                int nextAdress = (result-1)*4096;
 
-    
-        // std::cerr << " debug: The next  interior page start is" << nextAdress << "  "<<std::endl;
-         getLeafPage(database_file,nextAdress,leafAddresses);
+            
+                // std::cerr << " debug: The next  interior page start is" << nextAdress << "  "<<std::endl;
+                int j = getLeafPage(database_file,nextAdress,leafAddresses);
 
-          std::cerr << " total interior page  is" << num_table << " curr page is  "<<i<<" "<<std::endl;
+                std::cerr << " total interior page  is" << num_table << " curr page is  "<<i<<" "<<std::endl;
         }
     }
 
