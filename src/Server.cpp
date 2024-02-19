0a46cf39f6c5ead71f7c80d3e9fdf2a07442efc0
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 22:43:06 -0500

    pass first stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -203,7 +203,7 @@ int getRowData(std::vector<char> &database_file , unsigned long rowAddress,std::
       
        
 
-        if (index == 1 && printTables == true) {
+        if (index == 1 ) {
             std::cout <<currHeader<<"  "<<std::endl;
         }
         
