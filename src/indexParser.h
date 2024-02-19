a612f4d518c5c9c0b60a34bb742a1018cb601e20
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 21:56:01 -0500

    pass first stage

--- src/indexParser.h
+++ src/indexParser.h
@@ -10,10 +10,10 @@ const int COUNT_COMMAND = 1;
 const int COLUMNS_COMMAND = 2;
 
 // Constants for column types
-enum ColumnType {
-    COLUMN_TYPE_TEXT = 1,
-    COLUMN_TYPE_INTEGER = 2
-};
+// enum ColumnType {
+//     COLUMN_TYPE_TEXT = 1,
+//     COLUMN_TYPE_INTEGER = 2
+// };
 
 // Function declaration for printHello
 void printHello();
