2aee9027d080e81f2dd92fd0d1c5d9a2bfa4de56
Hamza Hafiz <hamzaha2@buffalo.edu>
2024-02-19 15:14:56 -0500

    tring to pass last stage

--- src/indexParser.cpp
+++ src/indexParser.cpp
@@ -223,12 +223,11 @@ void printIndexLeafPage(std::vector<char> &database_file , unsigned short rootPa
 
     unsigned short pageType = static_cast<unsigned char>(database_file[215887]);
     
-    if(pageType == 2)
-    {
-        
+      
      std::cout <<" The page start is" << i<<endl;
      std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(database_file[pageStart]) <<" "<<endl;
-    }
+     break;
+    
 
     }
     
