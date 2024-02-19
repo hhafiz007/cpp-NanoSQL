ae3b44e4a5823f7e8efd08cc93013b4412234f5a
Hamza Hafiz <hamzaha2@buffalo.edu>
2024-02-19 13:09:40 -0500

    tring to pass last stage

--- src/indexParser.cpp
+++ src/indexParser.cpp
@@ -217,7 +217,7 @@ void printIndexLeafPage(std::vector<char> &database_file , unsigned short rootPa
 
 
 
-    for(int i = 0 ; i < 13000;i+=1)
+    for(int i = 3620 ; i < 3635;i+=1)
     {
     unsigned long pageStart = i;
 
