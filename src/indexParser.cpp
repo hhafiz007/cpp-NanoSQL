9e763f44f26db9be1b2ea8ab1b99c5195a133b9e
Hamza Hafiz <hamzahafiz@Hamzas-MBP.lan>
2024-02-18 23:39:15 -0500

    pass first stage

--- src/indexParser.cpp
+++ src/indexParser.cpp
@@ -11,29 +11,160 @@
 #include<algorithm>
 using namespace std;
 
+const interiorIndexPage = 2;
+
+
+int processRowData(std::vector<char> &database_file , unsigned long rowAddress,vector<unsigned long> rowIds){
+    unsigned long next = rowAddress;
+    std::vector<int> header; 
+    next = processVarInt(database_file,rowAddress);
+   
+    next = processHeader(database_file,next,header);
+    int index = 0;
+    
+    std::vector<std::string> currRow;
+   
+    for (int element : header) {
+       
+        if (element >= 13) {
+            element = std::ceil((element -13)/2);
+        }
+   
+    
+        unsigned long startByte = next;
+        unsigned long  endExclusive = next + element;
+      
+
+        std:: string currHeader;
+        while (startByte < endExclusive) {
+            currHeader = currHeader + database_file[startByte];
+            startByte+=1;
+        }
+
+        std:: cerr << " cur header from index" << currHeader<<std::endl;
+
+         if (index == 0) {
+            // std::cerr << "cur row is " <<result<<"  "<<std::endl;
+             currRow.push_back(std::to_string(result));
+        }
+        else{
+             currRow.push_back(currHeader);
+            
+        }
+
+      
+       
+
+        if (index == 1 && printTables == true) {
+            std::cout <<currHeader<<"  "<<std::endl;
+        }
+        
+       
+         
+        next = endExclusive;
+        index+=1;
+    }
+
+    // std::cerr <<" total colucolsmns are are" << currRow.size() << std::endl;
+
+   
+    tableData.push_back(currRow);
+
+
+
+
+    return next;
+
+    
+
+
+
+
+
+
+
+
+}
+
+
+
+void parseInteriorIndexPages(std::vector<char> &database_file,unsigned long pageStart,vector<unsigned long> rowIds){
+
+
+    unsigned short pageType = static_cast<unsigned char>(database_file[pageStart]);
+
+    unsigned long startAddress = pageStart;
+    if (int(pageType) == interiorIndexPage) {
+            
+            unsigned short byte1 = static_cast<unsigned char>(database_file[startAddress+3]);
+            unsigned short byte2 = static_cast<unsigned char>(database_file[startAddress+4]);
+            num_table =   (byte1 << 8) | (byte2);
+            start = startAddress+12;
+            std::vector<unsigned long> cellAddress;
+             for (int i = 0; i < num_table; i++) {
+                int startIndex = (2*i)+start; 
+                unsigned long byte1 = static_cast<unsigned char>(database_file[startIndex]);
+                unsigned long byte2 = static_cast<unsigned char>(database_file[startIndex+1]);
+                unsigned long page_address =   (byte1 << 8) | (byte2);
+                cellAddress.push_back(pageStart+page_address);
+             }
+
+             for (int i = 0; i < cellAddress.size(); ++i) {
+
+
+            
+
+                unsigned long result1 = static_cast<unsigned char>(database_file[cellAddress[i]]);
+                unsigned long  result2 = static_cast<unsigned char>(database_file[cellAddress[i]+1]);
+                unsigned long  result3 = static_cast<unsigned char>(database_file[cellAddress[i]+2]);
+                unsigned long  result4 = static_cast<unsigned char>(database_file[cellAddress[i]+3]);
+
+                
+               
+                unsigned long  leftPointer =   ((result1 << 24) | (result2 << 16) | (result3 << 8) | (result4));
+
+                next = processRowData(database_file,cellAddress[i]+4,rowIds)
+
+
+
+
+
+
+    
+            
+        }
+        
+
+            
+
+        }
+    else {
+
+
+    }
+
+
+
+
+
+
+
+}
+
 void printIndexLeafPage(std::vector<char> &database_file , unsigned short rootPage,string indexValue) {
     
    
     unsigned long pageStart = (rootPage)*4096;
    
-    unsigned short pageType = static_cast<unsigned char>(database_file[pageStart]);
-
-    cout << " welcome to root page" << rootPage <<"  "<<indexValue<<"  "<<pageStart<<endl;
-
-
     
 
-        // std::vector<unsigned long> leafAddresses;
+    cout << " welcome to root page" << rootPage <<"  "<<indexValue<<"  "<<pageStart<<endl;
+    std::vector<unsigned long> rowIds;
 
-        // if (int(pageType) == interiorTablePage) {
-        //     debugStage = true;
-        //     pageStart = getLeafPage(database_file,pageStart,leafAddresses);
-        //     start = pageStart+8;
 
-        // }
-        // else{
-        //     leafAddresses.push_back((pageStart));
-        // }
+
+
+        
 
 
         // for (unsigned long startAddress: leafAddresses){
